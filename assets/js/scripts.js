8a73f99c70bea366fa36134f740b56623fccad26
ricardoarevalo1 <1ricardoarevalo@gmail.com>
2020-04-23 13:46:47 -0500

    Fixed Scrolling Menu

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,23 @@
 $( document ).ready(function() {
 
-    console.log("Hola");
+    try {
+        $(window).scroll(function (event) {
+            var scroll = $(window).scrollTop(); // Identifico el valor de scroll
+            
+            if (scroll > 120) { //Si el scroll es superior a 90px el header se hace fijo
+                $("nav.menu").addClass("fixedMenu");
+                console.log("Fixed");
+                
+                
+            } else {// Al volver al top, el header se normaliza
+                $("nav.menu").removeClass("fixedMenu");
+                console.log("unFixed");
+            }
+           
+ 
+        });
+        
+        } catch (error) {
+    
+        };
 });
