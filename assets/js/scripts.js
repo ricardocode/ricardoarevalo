714038bc34fa4f6c9ef41f668aa3e36b7af3566b
ricardoarevalo1 <1ricardoarevalo@gmail.com>
2020-04-29 14:44:50 -0500

    Fix home - animate - trying includes - start portfolio

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,20 +4,19 @@ $( document ).ready(function() {
         $(window).scroll(function (event) {
             var scroll = $(window).scrollTop(); // Identifico el valor de scroll
             
-            if (scroll > 120) { //Si el scroll es superior a 90px el header se hace fijo
+            if (scroll > 120) { //Si el scroll es superior a 120px el menú se hace fijo
                 $("nav.menu").addClass("fixedMenu");
-                console.log("Fixed");
-                
+                //console.log("Fixed");   
                 
             } else {// Al volver al top, el header se normaliza
                 $("nav.menu").removeClass("fixedMenu");
-                console.log("unFixed");
+                //console.log("unFixed");
             }
-           
  
         });
+        } catch (error) {};
         
-        } catch (error) {
-    
-        };
+        $(function(){
+            $("#footer").load("inc/footer.html");     
+        });
 });
