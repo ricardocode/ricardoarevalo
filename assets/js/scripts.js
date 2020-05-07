6ae6d258fe100854b6f5f9c91ce8991008f73d51
ricardoarevalo1 <1ricardoarevalo@gmail.com>
2020-05-07 14:16:19 -0500

    Edit Portfolio, create contact view and project view

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -19,4 +19,20 @@ $( document ).ready(function() {
         $(function(){
             $("#footer").load("inc/footer.html");     
         });
+
+
+        var textarea = document.querySelector('textarea');
+
+        textarea.addEventListener('keydown', autosize);
+                    
+        function autosize(){
+        var el = this;
+        setTimeout(function(){
+            el.style.cssText = 'height:auto';
+            // for box-sizing other than "content-box" use:
+            // el.style.cssText = '-moz-box-sizing:content-box';
+            el.style.cssText = 'height:' + el.scrollHeight + 'px'+ '!important';
+        },0);
+        }
 });
+    
